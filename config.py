b29a6a8d4b7ecd4895a107a6b3adc8a82aa29b79
SalmaanShire365 <salmaanshire@gmail.com>
2025-11-24 17:27:39 -0600

    Use environment variables for config + updated requirments

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-MON_URI = 'mongodb+srv://shiresalmaan0:307112@cluster0.dudjyab.mongodb.net/?retryWrites=true&w=majority'
+import os
 
-DATABASE_NAME = 'bookshelf_db'
-
-DEBUG_MODE = True
-
-
+MON_URI = os.environ.get('MON_URI', 'mongodb+srv://shiresalmaan0:307112@cluster0.dudjyab.mongodb.net/?retryWrites=true&w=majority')
+DATABASE_NAME = os.environ.get('DATABASE_NAME', 'bookshelf_db')
+DEBUG_MODE = os.environ.get('DEBUG_MODE', 'True') == 'True'
